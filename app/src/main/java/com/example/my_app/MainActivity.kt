0b05b67b730406fbae19ce17aac7e5b372dc831d
Argen2002 <Esenaliev_a@iuca.kg>
2023-05-22 18:20:22 +0600

    make navigation

--- app/src/main/java/com/example/my_app/MainActivity.kt
+++ app/src/main/java/com/example/my_app/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.my_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)//вызов родительской реализации
         setContentView(R.layout.activity_main)//file gde my zapisyvaly xml code/interface
+
+
+        val bottomView=findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController=findNavController(R.id.container)
+
+        bottomView.setupWithNavController(navController)
+
     }
 
     override fun onStart() {
